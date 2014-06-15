54467e3474309fad752a32da580ea115abb49193
Natalia Fisher <nacf@nacf-macbookpro2.roam.corp.google.com>
2014-06-15 15:59:23 -0700

    Initial Commit

---
+++ tips/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tips
+//
+//  Created by Natalia Fisher on 6/15/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
